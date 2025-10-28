92743ae983f1fb1a9ef2421746c11c828f6f2249
tonton.kina <sweetie.beomgyu@gmail.com>
2025-10-28 05:21:51 +0300

    Реализованы функции encrypt_vigenere и decrypt_vigenere

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -9,7 +9,20 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    keyword = keyword.upper()
+    key_length = len(keyword)
+
+    for i, char in enumerate(plaintext):
+        if char.isalpha():
+            key_char = keyword[i % key_length]
+            shift = ord(key_char) - ord('A')
+
+            if char.isupper():
+                ciphertext += chr((ord(char) - ord('A') + shift) % 26 + ord('A'))
+            else:
+                ciphertext += chr((ord(char) - ord('a') + shift) % 26 + ord('a'))
+        else:
+            ciphertext += char
     return ciphertext
 
 
@@ -24,5 +37,18 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    keyword = keyword.upper()
+    key_length = len(keyword)
+
+    for i, char in enumerate(ciphertext):
+        if char.isalpha():
+            key_char = keyword[i % key_length]
+            shift = ord(key_char) - ord('A')
+            
+            if char.isupper():
+                plaintext += chr((ord(char) - ord('A') - shift) % 26 + ord('A'))
+            else:
+                plaintext += chr((ord(char) - ord('a') - shift) % 26 + ord('a'))
+        else:
+            plaintext += char
     return plaintext
