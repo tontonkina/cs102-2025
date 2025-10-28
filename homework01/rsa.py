0e501a1d0ee3fd7efb72a196b234832036a7b1ae
tonton.kina <sweetie.beomgyu@gmail.com>
2025-10-29 01:06:45 +0300

    Реализованы все функции RSA

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -64,11 +64,9 @@ def generate_keypair(p: int, q: int) -> Tuple[Tuple[int, int], Tuple[int, int]]:
     elif p == q:
         raise ValueError('p and q cannot be equal')
 
-    # n = pq
-    # PUT YOUR CODE HERE
+    n = p * q
 
-    # phi = (p-1)(q-1)
-    # PUT YOUR CODE HERE
+    phi = (p-1) * (q-1) 
 
     # Choose an integer e such that e and phi(n) are coprime
     e = random.randrange(1, phi)
