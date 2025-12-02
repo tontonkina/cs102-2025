2188c6b460dc412739a3ff996fdc3ca40c570a8e
tonton.kina <sweetie.beomgyu@gmail.com>
2025-12-02 09:49:44 +0300

    fixed colour

--- homework03/maze_gui.py
+++ homework03/maze_gui.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from typing import List
+from typing import List, Union
 from tkinter import ttk, messagebox
 from maze import bin_tree_maze, solve_maze, add_path_to_grid
 
@@ -12,16 +12,16 @@ def draw_cell(x, y, color, size: int = 10):
     canvas.create_rectangle(x, y, x1, y1, fill=color)
 
 
-def draw_maze(grid: List[List[str]], size: int = 10):
+def draw_maze(grid: List[List[Union[str, int]]], size: int = 10):
     for x, row in enumerate(grid):
         for y, cell in enumerate(row):
             if cell == " ":
-                color = 'White'
+                colour = "White"
             elif cell == "■":
-                color = 'black'
+                colour = "black"
             elif cell == "X":
-                color = "red"
-            draw_cell(y, x, color, size)
+                colour = "blue"
+            draw_cell(y, x, colour, size)
 
 
 def show_solution():
@@ -40,8 +40,14 @@ if __name__ == "__main__":
     CELL_SIZE = 10
     GRID = bin_tree_maze(N, M)
 
+    _, PATH = solve_maze(GRID)
+    while not PATH:
+        print("No solutions. Regenerating maze...")
+        GRID = bin_tree_maze(N, M)
+        _, PATH = solve_maze(GRID)
+
     window = tk.Tk()
-    window.title('Maze')
+    window.title("Maze")
     window.geometry("%dx%d" % (M * CELL_SIZE + 100, N * CELL_SIZE + 100))
 
     canvas = tk.Canvas(window, width=M * CELL_SIZE, height=N * CELL_SIZE)
@@ -51,4 +57,3 @@ if __name__ == "__main__":
     ttk.Button(window, text="Solve", command=show_solution).pack(pady=20)
 
     window.mainloop()
-
