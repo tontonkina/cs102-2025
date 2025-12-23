6f9515e14883b8b6b0ee0475e60cd84ac04bd975
tonton.kina <sweetie.beomgyu@gmail.com>
2025-12-23 19:59:09 +0300

    fixed

--- homework04/life_gui.py
+++ homework04/life_gui.py
@@ -6,8 +6,7 @@ import random
 from pathlib import Path
 
 import pygame
-from pygame.locals import (KEYDOWN, MOUSEBUTTONDOWN, QUIT, K_l, K_p, K_q, K_r,
-                           K_s)
+from pygame.locals import KEYDOWN, MOUSEBUTTONDOWN, QUIT, K_l, K_p, K_q, K_r, K_s
 
 from life import GameOfLife
 from ui import UI
